d109f8989bc761c736e5aa17e4dd7e40d6817fdb
y715459784 <715459784@qq.com>
2020-01-11 20:10:53 +0800

    骨架屏-基本编写

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='container'>
+      主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home' // devtools查看组件时  可以看到 对应的name名称
+}
+</script>
+
+<style>
+</style>
