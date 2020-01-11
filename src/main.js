8efcb02ca19cda32ec14e6a1ad323021b271133d
y715459784 <715459784@qq.com>
2020-01-11 19:58:36 +0800

    搜索按钮点击事件完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
